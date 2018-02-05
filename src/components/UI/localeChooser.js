f1a4d190189e20ff0f9de89f8e21098fac40ece4
rajivstha <stha.rajiv@gmail.com>
2018-02-05 14:12:28 +0545

    changes

--- src/components/UI/localeChooser.js
+++ src/components/UI/localeChooser.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {View, TouchableOpacity, Text} from 'react-native';
-import { Actions} from 'react-native-router-flux';
 import style from './style';
 
 class LocaleChooser extends Component {
@@ -10,7 +9,6 @@ class LocaleChooser extends Component {
         <View style={style.languageItem}>
           <TouchableOpacity onPress={() => {
             this.props.onChange('en');
-            Actions.refresh();
           }}>
             <Text
               style={[style.language, (this.props.activeLocale === 'en' ? style.activeLocaleChooserStyle : {})]}>EN</Text>
@@ -19,7 +17,6 @@ class LocaleChooser extends Component {
         <View style={style.languageItem}>
           <TouchableOpacity onPress={() => {
             this.props.onChange('np');
-            Actions.refresh();
           }}>
             <Text
               style={[style.language, (this.props.activeLocale === 'np' ? style.activeLocaleChooserStyle : {})]}>NP</Text>
